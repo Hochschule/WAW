7eb542a145490dd60d9e5889eaa2d49d69b73f4a
poule <pscheffczyk93@gmail.com>
2014-06-03 02:56:21 +0200

    verbesserung bzw. anpassung der Methode eingabeUeberpruefung() an die aufgabenstellung warnmeldung kommt jetzt nur einmal alle falschen felder werden auf einmal rot markiert cursor fokusiert auf erstes falsches feld
    TODO: bei teilweise richtiger eingabe beim 2ten mal wird sie zwar als richtig anerkannt allerdings bleibt der rote rand

--- Meilenstein4/JavaScript/indexEingabeFormatierung.js
+++ Meilenstein4/JavaScript/indexEingabeFormatierung.js
@@ -5,53 +5,68 @@ function eingabeUeberpruefung() {
     var matnr = $("input[name=matrikelnr]").val();
     var handy = $("input[name=handy]").val();
 
+    var gabFehler=false;
     var eingabe = false;
 
-    if (vorname.match("([a-z]|[A-Z])+")) {
+    if (vorname.match("([a-z]|[A-Z])+")&& vorname!="Bitte Vorname eingeben") {
+
         eingabe = true;
-        $(vorname).css("border-style: none");
+        //vorName.className = "keinFehler";    //eigentlich soll es die roten kasten bei verbesserung wegmachen funktioniert nicht weiß nicht warum
     } else {
+        if(gabFehler==false){  document.getElementById( 'vornameID' ).focus()}
         eingabe = false;
         var vorName = document.getElementById("vornameID");
-        vorName.className = "fehlerEingabe";
-        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+       vorName.className = "fehlerEingabe";
+       gabFehler=true;
+        //alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
-    if (name.match("([a-z]|[A-Z])+")) {
+    if (name.match("([a-z]|[A-Z])+")&& name!="Bitte Nachname eingeben") {
         eingabe = true;
     } else {
+        if(gabFehler==false){  document.getElementById( 'nameID' ).focus()}
         eingabe = false;
         var _Name = document.getElementById("nameID");
         _Name.className = "fehlerEingabe";
-        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        gabFehler=true;
+        //alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+    }
+    if (email.match("[^@]+@[^@]+.(de|org|net|com)")) {
+        eingabe = true;
+    } else {
+        if(gabFehler==false){  document.getElementById( 'emailID' ).focus()}
+        eingabe = false;
+        var _email = document.getElementById("emailID");
+        _email.className = "fehlerEingabe";
+        gabFehler=true;
+        //alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
     if (matnr.match("([0-9]{7})")) {
         eingabe = true;
     } else {
+        if(gabFehler==false){  document.getElementById( 'matnrID' ).focus()}
         eingabe = false;
         var _matnr = document.getElementById("matnrID");
         _matnr.className = "fehlerEingabe";
-        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        gabFehler=true;
+        //alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
     if (handy.match("01[1-9][0-9]([0-9]{7,8})")) {
         eingabe = true;
     } else {
+        if(gabFehler==false){  document.getElementById( 'handyID' ).focus()}
         eingabe = false;
         var _handy = document.getElementById("handyID");
         _handy.className = "fehlerEingabe";
-        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        gabFehler=true;
+        //alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
-    if (email.match("[^@]+@[^@]+.(de|org|net|com)")) {
-        eingabe = true;
-    } else {
-        eingabe = false;
-        var _email = document.getElementById("emailID");
-        _email.className = "fehlerEingabe";
+
+    if(gabFehler){
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
-
     return eingabe;
 }
