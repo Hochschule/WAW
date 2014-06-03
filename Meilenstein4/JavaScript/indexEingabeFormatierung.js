bad9aff919f17f95f9f4ea18d05d9aaa456dbdf5
Simmert1208 <sebastian_simmert2005@yahoo.de>
2014-06-03 03:17:59 +0200

    Sebastian Simmert Meilenstein 4 Fehlerhafte markierung verschwindet nach korrektur

--- Meilenstein4/JavaScript/indexEingabeFormatierung.js
+++ Meilenstein4/JavaScript/indexEingabeFormatierung.js
@@ -5,53 +5,70 @@ function eingabeUeberpruefung() {
     var matnr = $("input[name=matrikelnr]").val();
     var handy = $("input[name=handy]").val();
 
+    var eingabeVorname = false;
+    var eingabeName = false;
+    var eingabeEmail = false;
+    var eingabeMatnr = false;
+    var eingabeHandy = false;
     var eingabe = false;
 
     if (vorname.match("([a-z]|[A-Z])+")) {
-        eingabe = true;
-        $(vorname).css("border-style: none");
+        eingabeVorname = true;
+        var vorName = document.getElementById("vornameID");
+        vorName.className = "richtigeEingabe";
+
     } else {
-        eingabe = false;
+        eingabeVorname = false;
         var vorName = document.getElementById("vornameID");
         vorName.className = "fehlerEingabe";
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
     if (name.match("([a-z]|[A-Z])+")) {
-        eingabe = true;
+        eingabeName = true;
+        var _Name = document.getElementById("nameID");
+        _Name.className = "richtigeEingabe";
     } else {
-        eingabe = false;
+        eingabeName = false;
         var _Name = document.getElementById("nameID");
         _Name.className = "fehlerEingabe";
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
-
-    if (matnr.match("([0-9]{7})")) {
-        eingabe = true;
+    if (email.match("[^@]+@[^@]+.(de|org|net|com)")) {
+        eingabeEmail = true;
+        var _email = document.getElementById("emailID");
+        _email.className = "richtigeEingabe";
     } else {
-        eingabe = false;
+        eingabeEmail = false;
+        var _email = document.getElementById("emailID");
+        _email.className = "fehlerEingabe";
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+    }
+    if (matnr.match("([0-9]{7})")) {
+        eingabeMatnr = true;
+        var _matnr = document.getElementById("matnrID");
+        _matnr.className = "richtigeEingabe";
+    } else {
+        eingabeMatnr = false;
         var _matnr = document.getElementById("matnrID");
         _matnr.className = "fehlerEingabe";
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
     if (handy.match("01[1-9][0-9]([0-9]{7,8})")) {
-        eingabe = true;
+        eingabeHandy = true;
+        var _handy = document.getElementById("handyID");
+        _handy.className = "richtigeEingabe";
     } else {
-        eingabe = false;
+        eingabeHandy = false;
         var _handy = document.getElementById("handyID");
         _handy.className = "fehlerEingabe";
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
     }
 
-    if (email.match("[^@]+@[^@]+.(de|org|net|com)")) {
+    if((eingabeVorname==true)&&(eingabeName==true)&&(eingabeEmail==true)&&(eingabeMatnr==true)&&(eingabeHandy==true)){
         eingabe = true;
-    } else {
-        eingabe = false;
-        var _email = document.getElementById("emailID");
-        _email.className = "fehlerEingabe";
-        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-    }
-
+    }else{
+    eingabe = false;}
     return eingabe;
 }
