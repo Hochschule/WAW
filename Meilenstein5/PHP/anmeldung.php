317618927744324616e4ea8ce0e5ccf9fd2574b4
Simmert1208 <sebastian_simmert2005@yahoo.de>
2014-06-19 14:01:47 +0200

    Sebastian Simmert (1225624)                 Meilenstein 5 Aufgabe 1 fertiggestellt, evtl fehlen kleinigkeiten. Ordner Meilenstein5 muss dafür in xampp kopiert werden

--- Meilenstein5/PHP/anmeldung.php
+++ Meilenstein5/PHP/anmeldung.php
@@ -4,4 +4,35 @@
  * User: SimmertL
  * Date: 19.06.14
  * Time: 11:38
- */ 
+ */
+
+$name = $_POST;
+if($name["messe"]=="cebit")
+{
+    $my_file = 'C:\xampp\htdocs\Meilenstein5\Textfiles\cebit.txt';
+    $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+}elseif($name["messe"]=="conit")
+{
+    $my_file = 'C:\xampp\htdocs\Meilenstein5\Textfiles\conit.txt';
+    $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+}elseif($name["messe"]=="webtechcon")
+{
+    $my_file = 'C:\xampp\htdocs\Meilenstein5\Textfiles\webtech.txt';
+    $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+}
+
+
+fwrite($handle,$name["vorname"]);
+fwrite($handle," ");
+fwrite($handle,$name["name"]);
+fwrite($handle,", ");
+fwrite($handle,$name["matrikelnr"]);
+fwrite($handle,", ");
+fwrite($handle,$name["email"]);
+fwrite($handle,", ");
+fwrite($handle,$name["handy"]);
+fwrite($handle,", ");
+fwrite($handle,$name["studiengang"]);
+fclose($handle);
+
+?>
